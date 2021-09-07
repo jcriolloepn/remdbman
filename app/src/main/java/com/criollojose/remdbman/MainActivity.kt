45708a6c0e31c5a60eb4655d2cd19089561b97b0
LMINO20 <lenin.mino@epn.edu.ec>
2021-09-07 12:43:35 -0500

    Estilos de letras

--- app/src/main/java/com/criollojose/remdbman/MainActivity.kt
+++ app/src/main/java/com/criollojose/remdbman/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.criollojose.remdbman
 
 import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.criollojose.remdbman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var typeFace=Typeface.createFromAsset(assets, "fonts/SourceCodePro-Bold.ttf")
+
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val unlockButton=binding.unlockButton
@@ -15,5 +20,6 @@ class MainActivity : AppCompatActivity() {
             val goToListItem=Intent(this,ConectionList::class.java)
             startActivity(goToListItem)
         }
+        unlockButton.typeface =typeFace
     }
 }
