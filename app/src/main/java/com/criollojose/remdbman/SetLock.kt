236fdd29a1bd3b9545f95e9f71cb95b0cd9ba5ca
LMINO20 <lenin.mino@epn.edu.ec>
2021-09-07 11:50:17 -0500

    Clase Set Lock Final

--- app/src/main/java/com/criollojose/remdbman/SetLock.kt
+++ app/src/main/java/com/criollojose/remdbman/SetLock.kt
@@ -4,5 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SetLock : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_lock)
+    }
 }
